9f5cacb48f0f8304afbcfe4263fdd9e414e87efa
ftyszyx <whyzi@qq.com>
2025-10-25 15:48:54 +0800

    add tiktok

--- src/components/platforms.tsx
+++ src/components/platforms.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { useTranslations } from "@/i18n/client";
 import Script from "next/script";
+//font icon
 const platforms = [
   { key: "weibo", label: "新浪微博", iconId: "icon-sina" },
   { key: "sina", label: "新浪博客", iconId: "icon-sina" },
   { key: "qqzone", label: "QQ空间", iconId: "icon-qqzone" },
   { key: "qqsay", label: "QQ说说", iconId: "icon-qqzone" },
+  {key:"redbook", label: "小红书", iconId: "icon-xiaohongshu" },
+  // {key:"kuaishou", label: "快手", iconId: "icon-kuaishou" },
+  {key:"douyin", label: "抖音", iconId: "icon-douyin" },
 ];
 export default function Platforms() {
   const t = useTranslations();
   return (
     <section className="w-full py-8 md:py-12 lg:py-16">
-      <Script src="//at.alicdn.com/t/c/font_5026634_29qs6g3ypjh.js" strategy="afterInteractive" />
+      <Script src="//at.alicdn.com/t/c/font_5026634_57ft7qmsqv2.js" strategy="afterInteractive" />
       <div className="container px-4 md:px-6">
         <div className="text-center space-y-3">
           <h2 className="text-2xl md:text-4xl font-bold tracking-tight">{t('platforms.title')}</h2>
